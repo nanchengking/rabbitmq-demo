02054f7937435ede38f04c439aecd46fafec0455
baiding <baiding@pinduoduo.com>
2018-06-10 13:52:15 +0800

    format code

--- receive.py
+++ receive.py
@@ -1,8 +1,9 @@
 import pika
 import time
 
+
 def callback(ch, method, properties, body):
-    print(" [x] Received %r %s" % (body,time.time()))
+    print(" [x] Received %r %s" % (body, time.time()))
     time.sleep(1)
     #ch.basic_ack(delivery_tag = method.delivery_tag)
 
@@ -12,13 +13,13 @@ result = channel.queue_declare(exclusive=True)
 channel.exchange_declare(exchange='logs',
                          exchange_type='fanout')
 queue_name = result.method.queue
-print("my queue name is:%s"%queue_name)
+print("my queue name is:%s" % queue_name)
 channel.queue_bind(exchange='logs',
                    queue=queue_name)
 
-#channel.queue_declare(queue='hello')
-#保证一条消息，在消费者没有ack之前，不会有新的消息过来
-#channel.basic_qos(prefetch_count=1)
+# channel.queue_declare(queue='hello')
+# 保证一条消息，在消费者没有ack之前，不会有新的消息过来
+# channel.basic_qos(prefetch_count=1)
 
 channel.basic_consume(callback,
                       queue=queue_name,
--- send.py
+++ send.py
@@ -4,7 +4,7 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 channel.exchange_declare(exchange='logs',
                          exchange_type='fanout')
-channel.queue_declare(queue='task_queue',durable=True)
+channel.queue_declare(queue='task_queue', durable=True)
 # for i in range(0,20):
 # 	channel.basic_publish(exchange='logs',
 #                       routing_key='task_queue',
@@ -12,9 +12,9 @@ channel.queue_declare(queue='task_queue',durable=True)
 #                       properties=pika.BasicProperties(
 #                          delivery_mode = 2, # make message persistent
 #                       ))
-for i in range(0,10):
-	channel.basic_publish(exchange="logs",
-		routing_key="",
-		body="message number:"+str(i))
+for i in range(0, 10):
+    channel.basic_publish(exchange="logs",
+                          routing_key="",
+                          body="message number:" + str(i))
 print(" [x] Sent 'Hello World!'")
 connection.close()
